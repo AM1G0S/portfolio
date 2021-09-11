f55b67fae03684bdf7bf54febf145e54d004dc3a
Suleyman <43322192+AM1G0S@users.noreply.github.com>
2021-09-11 12:33:24 +0300

    9

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 $(function () {
 
-  $('.menu__btn').on('click', function () {
-    $('.menu__list').toggleClass('menu__list--active');
-    $(this).toggleClass('open');
+  $(".menu").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+
+
+  $('.burger, .menu__link').on('click', function () {
+    $('.menu').toggleClass('menu--active');
+    $('.burger').toggleClass('burger--open');
   });
 
   var mixer = mixitup(".portfolio__content");
