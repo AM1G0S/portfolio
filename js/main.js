fb9bf8f249bc52f26407f41729057570a4e97ff2
Suleyman <43322192+AM1G0S@users.noreply.github.com>
2021-09-13 20:16:01 +0300

    portfolioV1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function () {
 
-  $(".menu").on("click", "a", function (event) {
+  $(".menu, .header__content, .footer").on("click", "a", function (event) {
     event.preventDefault();
     var id = $(this).attr('href'),
       top = $(id).offset().top;
@@ -8,10 +8,45 @@ $(function () {
   });
 
 
-  $('.burger, .menu__link').on('click', function () {
+  $('.burger').on('click', function () {
     $('.menu').toggleClass('menu--active');
     $('.burger').toggleClass('burger--open');
+    $('body').toggleClass('scroll-lock');
   });
+
+  $('.menu__link').on('click', function () {
+    $('.menu').removeClass('menu--active');
+    $('.burger').removeClass('burger--open');
+    $('body').removeClass('scroll-lock');
+  });
+
+
+  // Липкий хедер
+  var header = $('.header__top');
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 140) {
+      header.addClass('header__top--fixed');
+    } else {
+      header.removeClass('header__top--fixed');
+    }
+  });
+  // </Липкий хедер>
+
+  // Кнопка вверх
+  var btn = $('#arrow-top');
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 700) {
+      btn.addClass('arrow-top--show');
+    } else {
+      btn.removeClass('arrow-top--show');
+    }
+  });
+  btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+  });
+  // </Кнопка вверх>
+
 
   var mixer = mixitup(".portfolio__content");
 
